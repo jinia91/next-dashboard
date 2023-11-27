e15132e3fc04037946d65845d35520b23a3aefcb
jinia91 <creee77@gmail.com>
2023-11-27 19:37:24 +0900

    next 실습

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,15 +12,14 @@ import {formatCurrency} from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
+    noStore()
+
     // Add noStore() here prevent the response from being cached.
     // This is equivalent to in fetch(..., {cache: 'no-store'}).
-    noStore();
     try {
         // Artificially delay a response for demo purposes.
         // Don't do this in production :)
 
-        console.log('Fetching revenue data...');
-        await new Promise((resolve) => setTimeout(resolve, 3000));
 
         const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -70,7 +69,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
     noStore()
-
     try {
         // You can probably combine these into a single SQL query
         // However, we are intentionally splitting them to demonstrate
